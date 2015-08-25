94b48601106858a290d5e6550f64a009736c1ed0
yershovkiril <dd041091eke@gmail.com>
2015-08-25 21:29:34 +0300

    files

---
+++ static/js/serverTask-1.js
@@ -0,0 +1,210 @@
+(function() {
+	'use strict';
+	var categoryName;
+	var toolsOff = 'n';
+	var textField;
+	var Todo;
+	var keyupTimeout;
+
+
+	function application(node) {
+		this.node = node;
+		$(node).append('<div class="filterContainer-1"><input class="filter"  placeholder="Фильтровать"><span class="lupa"></span><span class="text-delete">⊗</span></div><div class="table"></div>');
+
+		$.ajax({
+			url: 'http://localhost:5000/todos/',
+			success: function(response) {
+				Todo = response; 
+				response.forEach(function(element, index) {
+					application.prototype.addCategory(element, node);
+				});
+				$('.containerCategory table:first-child').addClass("active");
+				$('.containerCategory td:first-child').addClass("active");
+				$(node).append('<button>Новый список</button>');
+				$('.containerCategory button').on('click', function() {
+					toolsOff = 'y';
+					application.prototype.newList();
+					$(".table").scrollTop($(".table")[0].scrollHeight);
+				});
+			}
+		});
+
+		$('.table').on('click', function(e) {
+			if ($(e.target).hasClass('removal-tool')) {
+				application.prototype.removeCategory($(e.target));
+			} else if ($(e.target).hasClass('pencil-tool')) {
+				application.prototype.editCategory($(e.target));
+
+				$('.containerCategory .saveList').on('click', function(e) {
+					application.prototype.saveEdited($(e.target.parentElement));
+				});
+
+				$('.containerCategory .cancel').on('click', function(e) {
+					application.prototype.cancelEdit($(e.target.parentElement));
+				});
+			}
+			$('.table').on('click', function(e) {
+				if ($(e.target).hasClass('active') || $(e.target).hasClass('')) {
+					application.prototype.chooseCategory($(e.target));
+				}
+			});
+		});
+
+		$('.text-delete').on('click', function(e) {
+			$(e.target).parent().find('input.filter').val('');
+			$('.table table').remove();
+			Todo.forEach(function(element) {
+				application.prototype.addCategory(element);
+			});
+
+		});
+		
+		// search plugin
+		$('input.filter').on('keyup', function (event) {
+			application.prototype.searchSimilarCategory(event);	
+		});
+	}
+
+	application.prototype.searchSimilarCategory = function(event) {
+		clearTimeout(keyupTimeout);
+		if (event !== undefined) {
+			keyupTimeout = setTimeout(function() {
+				var text = event.target.value;
+				if (text !== '') {
+					text = new RegExp(text, "i");
+					$('.table table').remove();
+					Todo.forEach(function(element) {
+						if (text.test(element)) {
+							application.prototype.addCategory(element);
+						}
+					});
+				} else {
+					$('.table table').remove();
+					Todo.forEach(function(element) {
+						application.prototype.addCategory(element);
+					});
+				}
+			}, 700);
+		}
+	};
+
+	application.prototype.cancelEdit = function(target) {
+		var inputText = $('table input').val();
+		target[0].firstChild.textContent = textField;
+		$('.containerCategory button')[0].disabled = false;
+		$('table input').remove();
+		$('.saveList').remove();
+		$('.cancel').remove();
+		toolsOff = 'n';
+	};
+
+	application.prototype.saveEdited = function(target) {
+		var inputText = $('.table input').val();
+		if (inputText !== "") {
+			toolsOff = 'n';
+			$('.containerCategory button')[0].disabled = false;
+			$('table input').remove();
+			$('.saveList').remove();
+			$('.cancel').remove();
+			target[0].firstChild.textContent = inputText;
+			$.ajax({
+				type: 'delete',
+				url: 'http://localhost:5000/todos/' + textField
+			});
+			$.post('http://localhost:5000/todos/' + inputText, {
+				todo: JSON.stringify({
+					title: inputText + " todo list",
+					created: new Date().toString(),
+					tasks: []
+				})
+			});
+		}
+	};
+
+	application.prototype.toolsPanelBehaviour = function() {
+
+		var tables = document.querySelectorAll('.containerCategory table');
+		Array.prototype.forEach.call(tables, function(table) {
+			var tool = table.querySelector('.tools-panel');
+			var hiding;
+			table.addEventListener('mouseleave', function() {
+				hiding = setTimeout(function() {
+					tool.style.display = 'none';
+				}, 50);
+			});
+			table.addEventListener('mouseenter', function() {
+				if (table.className !== 'active' && toolsOff === 'n') {
+					clearTimeout(hiding);
+					tool.style.display = 'block';
+				}
+			});
+		});
+	};
+
+	application.prototype.removeCategory = function(target) {
+		var inputText = target[0].parentElement.previousSibling.textContent;
+		target[0].parentNode.parentElement.parentElement.parentElement.parentElement.remove();
+
+		$.ajax({
+			type: 'delete',
+			url: 'http://localhost:5000/todos/' + inputText,
+		});
+	};
+
+	application.prototype.editCategory = function(target) {
+		toolsOff = 'y';
+		textField = (target[0].parentElement.previousSibling.textContent);
+		target[0].parentElement.previousSibling.textContent = '';
+		target.parent().parent().append('<input value=' + textField + '><span class="saveList">Сохранить</span>  <span class="cancel">Отмена</span>');
+		$('.containerCategory button')[0].disabled = true;
+		target[0].parentElement.style.display = 'none';
+	};
+
+	application.prototype.addCategory = function(element, node) {
+		$('.table').append('<table><tr><td>' + element + '<div class="tools-panel"><span class="pencil-tool">✎</span><span class="removal-tool">X</span></div></td></tr></table>');
+		application.prototype.toolsPanelBehaviour();
+	};
+
+	application.prototype.newList = function() {
+		$('.containerCategory button')[0].disabled = true;
+		$('.table').append('<table><tr><td><input><span class="saveList">Сохранить</span>  <span class="cancel">Отмена</span></td></tr></table>');
+		$('.containerCategory .saveList').on('click', function(e) {
+			toolsOff = 'n';
+			var table = $('.containerCategory table');
+			var inputText = $(e.target.previousSibling).val();
+			if (inputText !== "") {
+				table[table.length - 1].remove();
+				application.prototype.addCategory(inputText);
+				$('.containerCategory button')[0].disabled = false;
+				$.post('http://localhost:5000/todos/' + inputText, {
+					todo: JSON.stringify({
+						title: inputText + " todo list",
+						created: new Date().toString(),
+						tasks: []
+					})
+				});
+			}
+		});
+		$('.containerCategory .cancel').on('click', function() {
+			var table = $('.containerCategory table');
+			table[table.length - 1].remove();
+			$('.containerCategory button')[0].disabled = false;
+			toolsOff = 'n';
+		});
+	};
+
+
+	application.prototype.chooseCategory = function(target) {
+
+		$('.active').removeClass('active');
+		target.addClass('active');
+		$('.active').parent().parent().parent().addClass('active');
+		$('.active > .tools-panel').css('display', 'none');
+		var text = target[0].firstChild.textContent;
+		var checkbox = document.querySelector('.show-done');
+		taskList.prototype.loadingTasks(text, checkbox.checked);
+
+	};
+
+	window.application = application;
+}());
---
+++ static/js/serverTask-2.1.js
@@ -0,0 +1,317 @@
+'use strict';
+
+(function(){
+	var toolsOff = 'n'; 
+	var checkbox = document.querySelector('.show-done');
+	var textField;
+	var inputText;
+	var TodoList;
+	var keyupTimeout;
+	var selectDay;
+	var selectMonth;
+	var selectYear;
+	var calendarDay;
+	var e;
+	
+	function taskList(node) {
+		this.node = node;
+		$(node).append('<div class="filterContainer-2"><input class="filter"  placeholder="Фильтровать"><span class="text-delete">⊗</span><span class="lupa"></span></div><input class="show-done" type="checkbox" checked>Показывать выполненные<div class="table1"><div class="loader"><img src="./ajax-loader.gif" alt="" /></div><div class="tableHead"></div><div class="tableTask"></div></div>');
+		$(node).append("<button>Добавить задачу</button>");
+		$('.tableHead').append('<table><tr><td class="taskItem head">Что надо сделать</td><td class="taskTerm head">Когда</td><td class="taskDone head">Сделано</td></tr></table>');
+		
+		$.ajax({
+			url: 'http://localhost:5000/todos/',
+			success: function(response) {
+				return response;
+			}
+		}).then(function(firstElem) {
+			return taskList.prototype.loadingTasks(firstElem[0]);
+		});
+
+		var checkbox = document.querySelector('.show-done');
+		checkbox.onchange = function() {
+			var nameActiveCategory = $('table.active').text();
+			nameActiveCategory = nameActiveCategory.substr(0, nameActiveCategory.length-2);
+			taskList.prototype.loadingTasks(nameActiveCategory, checkbox.checked);
+		};
+
+		$('.tasksList button').on('click', function() {
+					toolsOff = 'y';
+					taskList.prototype.newTask();
+					$(".table1").scrollTop($(".table1")[0].scrollHeight);
+				});
+		$('.tableTask').on('click', function(e){
+			if ($(e.target).hasClass('pencil-tool')) {
+				taskList.prototype.editTask(e);
+			} 
+		});
+		$('.text-delete').on('click', function(e) {
+			$(e.target).parent().find('input.filter').val('');
+			var nameActiveCategory = $('table.active').text();
+			nameActiveCategory = nameActiveCategory.substr(0, nameActiveCategory.length-2);
+			taskList.prototype.loadingTasks(nameActiveCategory);
+
+		});
+		$('input.filter').on('keyup', function (event) {
+			taskList.prototype.searchSimilarTask(event);	
+		});
+
+	}
+	
+	taskList.prototype.searchSimilarTask = function(event) {
+		clearTimeout(keyupTimeout);
+		if (event !== undefined) {
+			keyupTimeout = setTimeout(function() {
+				var text = event.target.value;
+				if (text !== '') {
+					text = new RegExp(text, "i");
+					$('.tableTask table').remove();
+					console.log(TodoList.tasks);
+					TodoList.tasks.forEach(function(element) {
+						if (text.test(element.description)) {
+							taskList.prototype.addTask(element);
+						}
+					});
+				} else {
+					var nameActiveCategory = $('table.active').text();
+					nameActiveCategory = nameActiveCategory.substr(0, nameActiveCategory.length - 2);
+					taskList.prototype.loadingTasks(nameActiveCategory);
+				}
+			}, 700);
+		}
+	};
+
+	taskList.prototype.cancelEditing = function(e) {
+		toolsOff = 'n';
+		$('textarea').parent().append('<span class="element-description">'+textField+'</span>');
+		$('.tasksList button')[0].disabled = false;
+		$('textarea').remove();
+		$('.taskItem .saveList').remove();
+		$('.taskItem .cancel').remove();
+
+	};
+
+	taskList.prototype.editTask = function(e) {
+		toolsOff = 'y';
+		textField = $(e.target).parent().parent().find('.element-description').text();
+		$(e.target).parent().parent().find('.element-description').remove();
+		$(e.target).parent().parent().append('<textarea></textarea><span class="saveList">Сохранить</span>  <span class="cancel">Отмена</span>');
+		$('textarea').val(textField);
+		$('.tasksList button')[0].disabled = true;
+		$('.toolsTask-panel').css('display', 'none');
+		$('.taskItem .saveList').on('click', function(e) {
+			taskList.prototype.saveEdited(e);
+		});
+		$('.taskItem .cancel').on('click', function(e) {
+			taskList.prototype.cancelEditing(e);
+		});
+	};
+
+	taskList.prototype.saveEdited = function(e) {
+		inputText = $('textarea').val();
+		if (inputText !== "") {
+			toolsOff = 'n';
+			$('textarea').parent().append('<span class="element-description">' + inputText + '</span>')
+			$('.tasksList button')[0].disabled = false;
+			$('textarea').remove();
+			$('.taskItem .saveList').remove();
+			$('.taskItem .cancel').remove();
+			taskList.prototype.checkTasks(TodoList);
+			taskList.prototype.showCalendar();
+		}
+	};
+
+	taskList.prototype.saveNewTask = function(inputText, nameActiveCategory) {
+		var table = $('.tasksList table');
+		if (inputText !== "") {
+			table[table.length - 1].remove();
+			taskList.prototype.addTask(inputText);
+			$('.tasksList button')[0].disabled = false;
+			$.getJSON('http://localhost:5000/todos/' + nameActiveCategory).then(function(TodoList) {
+				TodoList.tasks.push({
+					description: inputText,
+					done: false
+				})
+				console.log(TodoList);
+				return TodoList
+			}).then(function(TodoList) {
+				return $.ajax({
+					url: 'http://localhost:5000/todos/' + nameActiveCategory,
+					method: 'PUT',
+					data: {
+						todo: JSON.stringify(TodoList)
+					}
+				});
+			});
+		}
+	};
+
+	taskList.prototype.newTask = function() {
+		var nameActiveCategory = $('table.active').text();
+		nameActiveCategory = nameActiveCategory.substr(0, nameActiveCategory.length - 2);
+		$('.tasksList button')[0].disabled = true;
+		$('.tableTask').append('<table><tr><td><input><span class="saveList">Сохранить</span><span class="cancel">Отмена</span></td></tr></table>');
+		$('.tasksList .saveList').on('click', function(e) {
+			toolsOff = 'n';
+			var inputText = $(e.target.previousSibling).val();
+			taskList.prototype.saveNewTask(inputText, nameActiveCategory);
+			taskList.prototype.showCalendar();
+		});
+		$('.tableTask .cancel').on('click', function() {
+			var table = $('.tableTask table');
+			table[table.length - 1].remove();
+			$('.tasksList button')[0].disabled = false;
+			toolsOff = 'n';
+		});
+	};
+	taskList.prototype.loadingTasks = function(text, taskStatus) {
+		$('.tableTask table').remove();
+		$('.loader').css('display', 'block');
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'http://localhost:5000/todos/' + text, true);
+		xhr.send();
+		xhr.onreadystatechange = function() {
+			if (xhr.status === 200 && xhr.readyState === 4) {
+				var response = JSON.parse(xhr.response);
+				response.tasks.forEach(function(element) {
+					taskList.prototype.addTask(element, taskStatus);
+				});
+
+				$('.tableTask').css('display', 'block');
+				var $loader = $('.loader');
+				$loader.delay(100).fadeOut('fast');
+				$('.taskDone > input').on('click', function() {
+					taskList.prototype.checkTasks(TodoList);
+				});
+				taskList.prototype.showCalendar();
+			}
+		};
+		$.get('http://localhost:5000/todos/' + text, function(response) {
+			TodoList = response;
+		});
+	};
+	
+	taskList.prototype.addTask = function(element, taskStatus) {
+		var check;
+		if (taskStatus === false) {
+			if(element.done === false) {
+				check = '<input type="checkbox"';
+				$('.tableTask').append('<table><tr><td class="taskItem"><span class="element-description">' + (element.description || element) +'</span><div class="toolsTask-panel"><span class="pencil-tool">✎</span></div></td><td class="taskTerm"><span class="calendar">' + (element.term || '') +'</span></td><td class="taskDone">'+ 
+		check	+'</td></tr></table>');
+			}
+		} else {
+			if (element.done == true) {
+				check = '<input type="checkbox" checked>';
+			} else {
+				check = '<input type="checkbox"';
+			}
+			$('.tableTask').append('<table><tr><td class="taskItem"><span class="element-description">' + (element.description || element) +'</span><div class="toolsTask-panel"><span class="pencil-tool">✎</span></div></td><td class="taskTerm"><span class="calendar">' + (element.term || '') +'</span></td><td class="taskDone">'+ 
+		check	+'</td></tr></table>');
+		}
+		
+		$('.calendar').each(function(index, element) {
+			if (element.textContent) {
+				element.style.position = 'relative';
+				element.style.width = '80px';
+				element.style.background = 'none';
+			}
+		});
+
+		taskList.prototype.toolsPanelBehaviour();
+	};
+
+	
+	taskList.prototype.checkTasks = function(TodoList) {
+		var nameActiveCategory = $('table.active').text();
+		nameActiveCategory = nameActiveCategory.substr(0, nameActiveCategory.length - 2);
+		var tasks = [];
+		$('.element-description').each(function(index, element) {
+			tasks[index] = {};
+			tasks[index].description = element.textContent;
+		});
+		$('.taskDone input').each(function(index, element) {
+			tasks[index].done = element.checked;
+		});
+		TodoList.tasks = tasks;
+		$.ajax({
+			url: 'http://localhost:5000/todos/' + nameActiveCategory,
+			method: 'PUT',
+			data: {
+				todo: JSON.stringify(TodoList)
+			}
+		});
+	};
+
+	taskList.prototype.toolsPanelBehaviour = function() {
+
+		var tables = document.querySelectorAll('.tableTask .taskItem');
+		Array.prototype.forEach.call(tables, function(table) {
+			var tool = table.querySelector('.toolsTask-panel');
+			var hiding;
+			table.addEventListener('mouseleave', function() {
+				hiding = setTimeout(function() {
+					tool.style.display = 'none';
+				}, 50);
+			});
+			table.addEventListener('mouseenter', function() {
+				if (table.className !== 'active' && toolsOff === 'n') {
+					clearTimeout(hiding);
+					tool.style.display = 'block';
+				}
+			});
+		});
+	};
+	
+	taskList.prototype.showCalendar = function() {
+
+		$(".calendar").on('click', function(event) {
+			var nameActiveCategory = $('table.active').text();
+			nameActiveCategory = nameActiveCategory.substr(0, nameActiveCategory.length - 2);
+			$.get('http://localhost:5000/todos/' + nameActiveCategory, function(response) {
+				TodoList = response;
+				console.log(TodoList.tasks)
+			});
+			e = event;
+			$(event.target).css('background', 'none');
+			$(event.target).css('position', 'relative');
+			$(event.target).css('width', '80px');
+			$('.ui-state-active').removeClass('ui-state-active');
+			$('.hasDatepicker').css('display', 'block');
+			calendarDay = $('.ui-state-default');
+			$('.ui-datepicker-inline').on('click', function() {
+				calendarDay = $('.ui-state-default');
+				taskList.prototype.calendarDayBehaviour(calendarDay, nameActiveCategory);
+			});
+			taskList.prototype.calendarDayBehaviour(calendarDay, nameActiveCategory);
+
+		});
+	};
+	taskList.prototype.calendarDayBehaviour = function(calendarDay, nameActiveCategory) {
+
+			calendarDay.on('click', function(event) {
+				$('.ui-datepicker-current-day').removeClass('ui-datepicker-current-day');
+				$('.hasDatepicker').css('display', 'none');
+				selectMonth = +($(event.target).parent().attr('data-month')) + 1;
+				if (selectMonth.toString().length < 2) {
+					selectMonth = '0' + selectMonth;
+				}
+				selectYear = $(event.target).parent().attr('data-year');
+				selectDay = $(event.target).text();
+				$(e.target).text(selectDay + '-' + selectMonth + '-' + selectYear);
+				
+				$('.calendar').each(function(index, element) {
+					TodoList.tasks[index].term = element.textContent;
+				});
+				$.ajax({
+					url: 'http://localhost:5000/todos/' + nameActiveCategory,
+					method: 'PUT',
+					data: {
+						todo: JSON.stringify(TodoList)
+					}
+				});
+			});
+	};
+	window.taskList = taskList;
+}())
+
+
